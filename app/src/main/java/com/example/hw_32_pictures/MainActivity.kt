aa7644cc385105c76ba4875d5f1f8b96a1c4caec
VictoriaRyd <victoriarydina@gmail.com>
2022-04-10 12:16:43 +0300

    button added

--- app/src/main/java/com/example/hw_32_pictures/MainActivity.kt
+++ app/src/main/java/com/example/hw_32_pictures/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.hw_32_pictures
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hw_32_pictures.databinding.ActivityMainBinding
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun setImageURI(uri: URI){
+        val uri: Uri = Uri.parse("android.resource://$packageName/${binding.image}")
+        binding.image.setImageURI(null)
+        binding.image.setImageURI(uri)
     }
 }
